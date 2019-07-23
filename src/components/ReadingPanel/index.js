047ac3c983165757fef604b323536c7d1ee9df2c
carywill <carywill.k@gmail.com>
2019-07-24 00:01:01 +0800

    components folder is capitalized

---
+++ src/components/ReadingPanel/index.js
@@ -0,0 +1,148 @@
+import {message} from "antd";
+import classNames from "classnames";
+import React from "react";
+import ReactMarkdown from "react-markdown/with-html";
+import {connect} from "react-redux";
+
+import {Editor} from "..";
+import {
+  deletePost,
+  updateCurrentReadPostId,
+  updatePosts
+} from "../../actions";
+import styles from "./styles.module.scss";
+
+class ReadingPanel extends React.Component {
+  state = {
+    showEditor: false,
+    editorMode: "create" /** create | edit */
+  };
+
+  componentDidMount() {
+    this.initDisqus()
+  }
+
+  componentDidUpdate({currentReadPostId: prevCId}) {
+    if (prevCId !== this.props.currentReadPostId) {
+      this.initDisqus();
+    }
+  }
+
+  editorRef = React.createRef();
+
+  initDisqus = () => {
+    /** https://help.disqus.com/en/articles/1717137-use-configuration-variables-to-avoid-split-threads-and-missing-comments */
+    // eslint-disable-next-line
+    var disqus_config = function () {
+      this.page.identifier = this.props.currentReadPostId;
+    };
+
+    var d = document, s = d.createElement('script');
+    s.src = 'https://ljp-web.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', + new Date());
+    (d.head || d.body).appendChild(s);
+  }
+
+  createPost = () => {
+    this.setState({showEditor: true, editorMode: "create"});
+  };
+
+  editPost = (post) => {
+    if (!post) {
+      return message.error("没有选中的文章!")
+    }
+
+    this.setState({showEditor: true, editorMode: "edit"});
+    // Initialize editor
+    this.editorRef.current.init(post.title, post.body, post.categories);
+  };
+
+  deletePost = (post) => {
+    if (!post)
+      return message.error("当前没有选中的文章")
+    // TODO: prompt since it's a destructive action
+
+    // reset current read post id
+    const {dispatch} = this.props;
+    dispatch(updateCurrentReadPostId(""));
+    dispatch(deletePost(this.props.currentReadPostId));
+  };
+
+  dismissEditor = () => {
+    this.setState({showEditor: false});
+    // Update posts after create new post
+    this.props.dispatch(updatePosts());
+  };
+
+  renderContent = (post) => {
+    // No current reading post
+    if (!post) {
+      return (<div className={styles.placeholder}>
+        <span role="img" aria-label="jsx-a11y/accessible-emoji">
+          🍩
+        </span>
+        <span>Reading is the most wonderful thing in the world</span>
+      </div>)
+    }
+    // Got posts
+
+    return (
+      <>
+        <span className={styles.title}>{(post && post.title) || ""}</span>
+        <h6 style={{textAlign: "right"}}>{post.date.slice(0, 10)}</h6>
+        <ReactMarkdown
+          className="markdown-body"
+          escapeHtml={false}
+          source={(post && post.body) || "No content."}
+        />
+      </>
+    );
+  };
+
+  renderPostActionBar = () => {
+    const {posts, currentReadPostId: cId} = this.props;
+    const post = posts.all.find(
+      p => p.id === cId
+    );
+    const actions = ["deletePost", "editPost", "createPost"];
+
+    return (
+      <div className={styles.postActions}>
+        {actions.map(a => (
+          <button key={a} className={classNames(styles.basePostActionButton, styles[a])} onClick={() => this[a](post)}>
+            {a.replace("Post", "")}
+          </button>
+        ))}
+      </div>
+    );
+  };
+
+  render() {
+    const {posts, currentReadPostId: cId, user} = this.props;
+    const currentPost = posts.all.find(
+      p => p.id === cId
+    );
+
+    return (
+      <div className={styles.readingPanel}>
+        {this.renderContent(currentPost)}
+        {user.email &&
+          this.renderPostActionBar()}
+        <Editor
+          onCancel={this.dismissEditor}
+          visible={this.state.showEditor}
+          ref={this.editorRef}
+          mode={this.state.editorMode}
+          currentPost={currentPost}
+        />
+        <div id="disqus_thread" className={cId === "" ? styles.hide : styles.show}></div>
+      </div>
+    );
+  }
+}
+
+export default connect(({posts, currentReadPostId, user}) => ({
+  posts,
+  currentReadPostId,
+  user
+}))(ReadingPanel);
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,198 @@
+import {Input} from "antd";
+import classNames from "classnames";
+import React from "react";
+import {connect} from "react-redux";
+import {Link} from "react-router-dom";
+
+import {LoginModal, SearchResultModal} from "..";
+import {updateCurrentReadPostId, updatePosts} from "../../actions";
+import {capitalize} from "../utility"
+import styles from "./styles.module.scss";
+
+const {Search} = Input;
+class Sidebar extends React.Component {
+  state = {
+    isDropdown: false,
+    showSearchResultModal: false,
+    showLoginModal: false,
+    searchKeyword: "",
+    isViewAllPosts: false,
+    currentOpenedCategory: ""
+  };
+
+  componentDidMount() {
+    this.props.dispatch(updatePosts());
+  }
+
+  onSearch = keyword => {
+    this.setState({showSearchResultModal: true, searchKeyword: keyword});
+  };
+
+  dismissSearchResultModal = () => {
+    this.setState({showSearchResultModal: false});
+  };
+
+  dismissLoginModal = () => {
+    this.setState({showLoginModal: false});
+  };
+
+  toggleViewAllPosts = () => {
+    this.setState(prevState => {
+      return {isViewAllPosts: !prevState.isViewAllPosts};
+    });
+  };
+
+  toggleCategory = (category) => {
+    this.setState({
+      // 重复点击可 toggle
+      currentOpenedCategory:
+        this.state.currentOpenedCategory === category ? "" : category
+    })
+  }
+
+  onRowClick = (post) => {
+    this.props.dispatch(updateCurrentReadPostId(post.id));
+    this.setState({showSearchResultModal: false});
+  }
+
+  renderPostRow = (p, className) => {
+    return (<div
+      className={classNames(
+        className,
+        this.props.currentReadPostId === p.id && styles.active
+      )}
+      key={p._id}
+      onClick={() => this.onRowClick(p)}
+    >
+      <Link
+        style={{color: "inherit", textDecoration: "inherit"}}
+        to={{pathname: `/post/${p.date.slice(0, 10)}/${p.title}`, state: {id: p.id}}}
+      >
+        <time dateTime={p.date.slice(0, 10)} className={styles.postRowTitle}>{p.title}</time>
+      </Link>
+    </div>)
+  }
+
+  renderCategoryRow = (category, articles = []) => {
+    const count = articles.filter(a => a.categories.includes(category)).length;
+    const {currentOpenedCategory} = this.state;
+    return (
+      <React.Fragment key={category}>
+        <div
+          className={styles.categoryRow}
+          key={category}
+          onClick={() => this.toggleCategory(category)
+          }
+        >
+          <div className={styles.categoryRowText}>
+            <span>{capitalize(category)}</span>
+            <span className={styles.count}>{count}</span>
+          </div>
+          <span>
+            {currentOpenedCategory === category ? "∙" : "◦"}
+          </span>
+        </div>
+        {currentOpenedCategory === category && (
+          <div className={styles.postsByCategory}>
+            {articles
+              .filter(a => a.categories.includes(category))
+              .map(p => (
+                this.renderPostRow(p, styles.categoryPostRow)
+              ))}
+          </div>
+        )}
+      </React.Fragment>
+    )
+  }
+
+  renderPosts = (articles = []) => {
+    return (
+      <section className={styles.recentPosts}>
+        <div className={styles.postsHeaderBox}>
+          <span className={styles.recentPostsHeader}>近期文章</span>
+          <span
+            className={styles.viewAllPosts}
+            onClick={this.toggleViewAllPosts}
+          >
+            {this.state.isViewAllPosts ? "收起" : "查看全部"}
+          </span>
+        </div>
+        {articles.slice(0, this.state.isViewAllPosts ? articles.length : 3).map(p => (
+          this.renderPostRow(p, styles.postRow)
+        ))}
+      </section>
+    );
+  };
+
+  renderCategories = (articles = []) => {
+    const categories = articles.reduce((p, c) => {
+      return [...p, ...c.categories];
+    }, []);
+    const uniqueCategories = [...new Set(categories)];
+
+    return (
+      <section className={styles.recentPosts}>
+        <span className={styles.recentPostsHeader}>分类</span>
+        {uniqueCategories.map(category => this.renderCategoryRow(category, articles))}
+      </section>
+    );
+  };
+
+  renderHeader = () => {
+    return (
+      <nav>
+        {["近期文章" /** , "分类"*/].map(c =>
+          (
+            <span
+              className={classNames(styles.item, styles.mobile)}
+              onClick={() => this.setState({showSearchResultModal: true})}
+              key={c}
+            >
+              {c}
+            </span>
+          )
+        )}
+        <span
+          className={classNames(styles.item, styles.login)}
+          onClick={() => {
+            this.setState({showLoginModal: true});
+          }}
+        >
+          {this.props.user.status === "success"
+            ? this.props.user.email
+            : "登陆"}
+        </span>
+      </nav>
+    );
+  };
+
+  render() {
+    const {posts: articles} = this.props;
+    const {showLoginModal, showSearchResultModal, searchKeyword} = this.state;
+
+    return (
+      <div className={styles.sidebar}>
+        <LoginModal onCancel={this.dismissLoginModal} visible={showLoginModal} />
+        <SearchResultModal visible={showSearchResultModal} onCancel={this.dismissSearchResultModal} initialSearchKeyword={searchKeyword} />
+        {this.renderHeader()}
+        <Search
+          placeholder="搜索"
+          onSearch={value => this.onSearch(value)}
+          className={styles.searchbar}
+        />
+        <div className={styles.desktop}>
+          {this.renderPosts(articles.all)}
+        </div>
+        <div className={styles.desktop}>
+          {this.renderCategories(articles.all)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(({posts, user, currentReadPostId}) => ({
+  posts,
+  user,
+  currentReadPostId
+}))(Sidebar);
